d958d56d7a95990890b32978f563a57408b48860
LakshitaTak <134226199+LakshitaTak@users.noreply.github.com>
2023-07-24 23:29:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-//your JS code here. If required.
+function formatDate(date) {
+  const day = date.getDate();
+  const month = date.getMonth() + 1; // Months are zero-based, so we add 1
+  const year = date.getFullYear().toString().slice(-2);
+  const hours = date.getHours() % 12;
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  const ampm = hours >= 12 ? 'AM' : 'PM';
+
+  return `${month}/${day}/${year}, ${formatTime(hours)}:${formatTime(minutes)}:${formatTime(seconds)} ${ampm}`;
+}
+
+function formatTime(time) {
+  return time.toString().padStart(2, '0');
+}
+
+function updateTime() {
+  const currentDate = new Date();
+  const formattedDate = formatDate(currentDate);
+
+  const currentTime = document.getElementById("timer")
+  currentTime.innerText = formattedDate
+}
+
+setInterval(updateTime,1000)
